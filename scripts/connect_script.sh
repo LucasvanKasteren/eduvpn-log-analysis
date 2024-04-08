5bdc010f76c9452d873f12bdad3bfa37da470096
LucasvanKasteren <Lvankasteren4000@gmail.com>
2024-04-08 12:41:39 +0000

    ShellCheck

--- scripts/connect_script.sh
+++ scripts/connect_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "C" = "${VPN_EVENT}" ]; then	
-	OUTPUT=( $(convert_ip_to_geo "${VPN_ORIGINATING_IP}") )
+	mapfile -t OUTPUT < <(convert_ip_to_geo "${VPN_ORIGINATING_IP}")
 	if [ ! ${#OUTPUT[@]} -eq 0 ]; then
-		logger "LOCATION ${VPN_USER_ID} ${VPN_PROTO} ${OUTPUT[@]}"
+		logger "LOCATION ${VPN_USER_ID} ${VPN_PROTO}" "${OUTPUT[@]}"
 	fi
 else
 	logger "User ${VPN_USER_ID} disconnected"
--- scripts/run_impossible_travel.sh
+++ scripts/run_impossible_travel.sh
@@ -11,7 +11,7 @@ LOG_DIR_PATH=../logs/
 CRON_TIME=5
 
 create_directory() {
-	if [ ! -d $1 ]; 
+	if [ ! -d "$1" ]; 
 	then 
 		echo "Directory doesn't exist, creating now"
 		mkdir -p "$1"
@@ -23,9 +23,8 @@ create_directory() {
 #Create log directory
 create_directory "$LOG_DIR_PATH"
 
-#Capture time variables for unique file names
+#Capture time variable for unique file names
 TIME_NOW=$(date +"%d-%m-%y_%H:%M")
-TIME_EARLY=$(date -d "-$CRON_TIME min" "+%d-%m-%y_%H:%M")
 
 #Capture journal logs
 journalctl --no-pager -t vpn-user-portal -t www-data --since "1 hour ago" -o json > "$LOG_DIR_PATH"journal-logs.json
@@ -34,37 +33,23 @@ journalctl --no-pager -t vpn-user-portal -t www-data --since "1 hour ago" -o jso
 sudo wg show all > "$LOG_DIR_PATH"wireguard-peers.txt
 
 #Run python script 
-OUTPUT=($( ./env/bin/python3 impossible_travel.py "$LOG_DIR_PATH"journal-logs.json $DB_PATH "$LOG_DIR_PATH"wireguard-peers.txt "$LOG_DIR_PATH"outfile_"$TIME_NOW".json ))
+mapfile -t OUTPUT < <(./env/bin/python3 impossible_travel.py "$LOG_DIR_PATH"journal-logs.json $DB_PATH "$LOG_DIR_PATH"wireguard-peers.txt "$LOG_DIR_PATH"outfile_"$TIME_NOW".json)
 
 #Notify host if an impossible travel occurred
 if echo "${OUTPUT[@]}" | grep -q "True"; then
-	mailx -s "WARNING: Impossible Travel Detected" $LOGNAME@$HOSTNAME <<< "Impossible travel has occured on your server, please check the log files for additional information"
+	mailx -s "WARNING: Impossible Travel Detected" "$LOGNAME"@"$HOSTNAME" <<< "Impossible travel has occured on your server, please check the log files for additional information"
 fi
 
 #Remove logged wireguard data
 rm "$LOG_DIR_PATH"wireguard-peers.txt
 
 #Check if cronjob already exists for current user before adding it
-CRONJOB_EXISTS=$(crontab -u $LOGNAME -l 2>/dev/null | grep -c "run_impossible_travel.sh")
-if [ $CRONJOB_EXISTS -eq 0 ]; 
+CRONJOB_EXISTS=$(crontab -u "$LOGNAME" -l 2>/dev/null | grep -c "run_impossible_travel.sh")
+if [ "$CRONJOB_EXISTS" -eq 0 ]; 
 then
 	echo "Trying to add cronjob"
-	(crontab -l 2>/dev/null; echo "*/$CRON_TIME * * * * cd $HOME/debian-eduvpn-server/scripts && ./run_impossible_travel.sh > $HOME/debian-eduvpn-server/logs/cronjob.log 2>&1") | crontab -u $LOGNAME -
+	(crontab -l 2>/dev/null; echo "*/$CRON_TIME * * * * cd $HOME/debian-eduvpn-server/scripts && ./run_impossible_travel.sh > $HOME/debian-eduvpn-server/logs/cronjob.log 2>&1") | crontab -u "$LOGNAME" -
 	echo "Cronjob added."
 else
 	echo "Cronjob already exists."
 fi
-: '
-#Compare the current and previous output files and print the differences
-PREVIOUS_OUTPUT="$LOG_DIR_PATH"outfile_"$TIME_EARLY".json
-
-if [ -f "$PREVIOUS_OUTPUT" ];
-then
-	echo "Comparing previous and current outputs:"
-	diff $PREVIOUS_OUTPUT "$LOG_DIR_PATH"outfile_"$TIME_NOW".json
-else
-	echo "No previous output file found for comparison."
-fi
-'
-#cp "$LOG_DIR_PATH"outfile_"$TIME_NOW".json "$PREVIOUS_OUTPUT"
- 
